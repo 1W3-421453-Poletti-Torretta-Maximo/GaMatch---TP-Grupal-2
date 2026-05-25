e472ed7529b0e9611e4a8c2a652b06a76ccbb344
Marcos <421343@tecnicatura.frc.utn.edu.ar>
2026-05-25 01:10:07 -0300

    coso

--- backend/src/socket/handlers.ts
+++ backend/src/socket/handlers.ts
@@ -29,8 +29,8 @@ export function registerSocketHandlers(io: Server): void {
     socket.join(userId);
 
     // Mark user online in Neo4j
-    getSession().run(Q.SET_USER_ONLINE, { id: userId, isOnline: true }).then((s) => s.close?.());
-
+    const onlineSession = getSession();
+    onlineSession.run(Q.SET_USER_ONLINE, { id: userId, isOnline: true }).then(() => onlineSession.close());
     // Join a chat room
     socket.on('join_room', (roomId: string) => {
       socket.join(roomId);
@@ -63,9 +63,8 @@ export function registerSocketHandlers(io: Server): void {
     });
 
     socket.on('disconnect', () => {
-      getSession()
-        .run(Q.SET_USER_ONLINE, { id: userId, isOnline: false })
-        .then((s) => s.close?.());
+      const onlineSession = getSession();
+      onlineSession.run(Q.SET_USER_ONLINE, { id: userId, isOnline: true }).then(() => onlineSession.close());
     });
   });
 }
